7f3323ec5dfe12b55f60bbf4e93c3adff5a1ded3
Artem <somemail2344@gmail.com>
2019-03-03 23:20:28 +0200

    Changed test

--- src/test/java/TryJenkins.java
+++ src/test/java/TryJenkins.java
@@ -1,15 +1,18 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TryJenkins {
-
-    public static String chromeDriverPath = "/usr/bin/chromedriver";
+        public static WebDriver driver;
 
         @Test
         public void launchTest() {
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-            WebDriver driver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            options.addArguments("window-size=1200x600");
+            driver = new ChromeDriver(options);
             driver.get("https://www.google.com/");
             driver.quit();
         }
